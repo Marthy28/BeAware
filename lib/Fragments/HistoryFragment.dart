fdbfb8434b1c769b2cf5ad2fc0fcb12666b3942d
Maxime Grand <mgrand@apsdigit.lan>
2021-03-11 15:44:14 +0100

    first commit

--- lib/Fragments/HistoryFragment.dart
+++ lib/Fragments/HistoryFragment.dart
@@ -1,16 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:be_aware/Util/global.dart';
 
-class HistoryFragment extends StatelessWidget
-{
+class HistoryFragment extends StatefulWidget {
+  @override
+  _HistoryFragment createState() => _HistoryFragment();
+}
+
+class _HistoryFragment extends State<HistoryFragment> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center (
-        child: Text ("History"),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            _listPersonne(context),
+          ],
+        ),
       ),
     );
-    throw UnimplementedError();
   }
 
+  @override
+  Widget _listPersonne(BuildContext context) {
+    return Container(
+        child: FutureBuilder(
+            future: provider.getHistory(),
+            builder: (BuildContext, snapshot) {
+              if (snapshot.hasData && snapshot.data.length > 0) {
+                return ListView.builder(
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      return itemHistory(context, snapshot.data.docs[index]);
+                    });
+              }
+            }));
+  }
+
+  Widget itemHistory(BuildContext context, DocumentSnapshot document) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Text(
+        document.data()["user"],
+        textAlign: TextAlign.left,
+        style: Theme.of(context).textTheme.bodyText1.copyWith(
+            fontSize: 28.0, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      Text(
+        document.data()["date"],
+        textAlign: TextAlign.left,
+        style: Theme.of(context).textTheme.bodyText1.copyWith(
+            fontSize: 28.0, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+    ]);
+  }
 }
