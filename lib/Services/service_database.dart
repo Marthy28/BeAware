4a09c87f0a2610fb6498f0070b330f528d4bbfd8
Maxime Grand <mgrand@apsdigit.lan>
2021-03-11 15:44:14 +0100

    first commit

--- lib/Services/service_database.dart
+++ lib/Services/service_database.dart
@@ -2,10 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class DBProvider {
   main();
+  Future<List<DocumentSnapshot>> getHistory();
 }
 
 class DataBase implements DBProvider {
   FirebaseFirestore databaseReference;
+
+  Future<List<DocumentSnapshot>> getHistory() async {
+    QuerySnapshot snap = await databaseReference.collection('history').get();
+    return snap.docs;
+  }
 
   main() async {
     print("Opening connection ...");
