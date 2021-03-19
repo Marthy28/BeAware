353b6cea9ebc1306729cb17e808f836754ab301c
thomas_mary <tmary@oui.sncf>
2021-03-19 10:19:07 +0100

    CORRECTION DE LA SHAME

--- lib/Items/Profil.dart
+++ lib/Items/Profil.dart
@@ -2,6 +2,7 @@ class Profil {
   String firstName, lastName;
 
   Profil.fromProfilData(Map<String, dynamic> data) {
+    if (data == null) return;
     this.firstName = data['firstName'];
     this.lastName = data['lastName'];
   }
